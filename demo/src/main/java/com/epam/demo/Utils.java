a529884e2473178efb6c3d3790b08a3caf2770cd
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-06-22 00:05:38 +0500

    Utils class checkout

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,16 +1,25 @@
 package com.epam.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("-11.1");
+        System.out.println(isAllPositiveNumbers(list));
+    }
     public static boolean isAllPositiveNumbers(List<String> args) {
-        for(String s : args){
-            if (Integer.parseInt(s) < 0){
+        for (String s : args) {
+            try {
+                double number = Double.parseDouble(s);
+                if (number <= 0) {
+                    return false;
+                }
+            } catch (NumberFormatException e) {
                 return false;
-            } else {
-                return true;
             }
         }
-        return false;
+        return true;
     }
 }
